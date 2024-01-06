f01a3558e0accc6e1c06c9ebae07d58fe3a35406
icymanred <reuben.c_p@outlook.com>
2024-01-06 22:40:25 +0000

    Add project files.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,37 @@
+// ConsoleApplication5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+extern "C" {
+#include "Lua5/include/lua.h"
+#include "Lua5/include/lauxlib.h"
+#include "Lua5/include/lualib.h"
+#include "Lua5/include/lauxlib.h"
+
+}
+
+#pragma comment(lib,"Lua5/Lua54.lib")
+const char* str =  "Hewo baby";
+int main(int argc) {
+	
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+	
+
+	return 0;
+
+	
+ 
+	
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
